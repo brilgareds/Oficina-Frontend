7d78efe70ea12694572f921c2753306db329e4b1
Julian Andres Realpe <julian.realpe@visionymarketing.com.co>
2021-09-06 17:58:35 -0500

    Consulta para saber que menus estan completados 06/09/2021

--- src/components/menuOV/dto/menuOV.dto.ts
+++ src/components/menuOV/dto/menuOV.dto.ts
@@ -1,3 +1,13 @@
-
+import { IsNotEmpty, IsNumber, Max } from "class-validator";
 export class MenuOVDto {
+
+  @IsNotEmpty()
+  @IsNumber()
+  CODIGO_EMPRESA: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Max(9999999999)
+  NRO_DOCUMENTO: number;
+  
 }
