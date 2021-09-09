a9b91698f0397ad7359adef75ade1da042c0d5b2
develop-joseavila <86316083+develop-joseavila@users.noreply.github.com>
2021-09-08 19:19:39 -0500

    update incapacity module 2021-09-08
    update incapacity module 2021-09-08

--- src/components/incapacity/repositories/impl/mssql/incapacity.repository.ts
+++ src/components/incapacity/repositories/impl/mssql/incapacity.repository.ts
@@ -1,5 +1,8 @@
 import { mssqlBiplus, mssqlEsmad, mssqlKactus } from "../../../../../services/mssql";
+import { getDateToday } from "../../../../common/helpers/global";
 import { IncapacityhRepository } from "../../incapacity.repository";
+
+
 
 export class IncapacityMSSQLRepository implements IncapacityhRepository {
 
@@ -40,12 +43,83 @@ export class IncapacityMSSQLRepository implements IncapacityhRepository {
 
   }
 
-  public async saveDisabilityFiling(allData: any): Promise<any> {
+  public async searchLastIdIncapacity(): Promise<any> {
 
-    console.log("saveDisabilityFiling");
+    const pool = await mssqlEsmad;
+    const result = await pool.query`
+                          SELECT 
+                              DISTINCT IDENT_CURRENT('dbo.ESMAD_INCAPACIDADES') INCAP_CODIGO
+                          FROM 
+                              dbo.ESMAD_INCAPACIDADES
+    `;
 
-   
-    return true;
+    return result.recordset[0];
+
+  }
+
+  public async saveDisabilityFiling(cedula: any, nombre: any, telefono: any, mail: any, eps: any, otraEnti: any, otraEntidad: any, tipInca: any, fechaInicio: any, fechaFin: any, empresa: any, prorroga: any): Promise<any> {
+
+    try {
+
+      const now = getDateToday(false);
+      const pool = await mssqlEsmad;
+      const query = `
+                          INSERT INTO 
+                            dbo.ESMAD_INCAPACIDADES
+                            ( 
+                                INCAP_CEDULA, 
+                                INCAP_NOMBRE, 
+                                INCAP_TELEFONO, 
+                                INCAP_EMAIL, 
+                                INCAP_EPS,
+                                INCAP_OTRA_ENTID, 
+                                INCAP_OTRA_ENTID_NOMBRE, 
+                                INCAP_TIPO, 
+                                INCAP_FECHA_INI, 
+                                INCAP_FECHA_FIN, 
+                                USUARIO_CREACION, 
+                                FECHA_CREACION,
+                                USUARIO_MODIFICACION, 
+                                FECHA_MODIFICACION, 
+                                ESTADO, 
+                                INCAP_EMPRESA, 
+                                INCAP_PRORROGA 
+                            )
+                            VALUES 
+                            ( 
+                                ${cedula}, 
+                                '${nombre}', 
+                                '${telefono}', 
+                                '${mail}', 
+                                '${eps}', 
+                                ${otraEnti}, 
+                                ${otraEntidad}, 
+                                ${tipInca}, 
+                                '${fechaInicio}', 
+                                '${fechaFin}', 
+                                '${cedula}', 
+                                '${now}', 
+                                '${cedula}', 
+                                '${now}', 
+                                1, 
+                                ${empresa}, 
+                                ${prorroga} 
+                            )
+        `;
+
+      const result = await pool.query(query);
+
+      if (result.rowsAffected) {
+        return this.searchLastIdIncapacity();
+      }
+
+      return false;
+    } catch (error) {
+
+      console.log(error);
+
+      return false;
+    }
 
   }
 
