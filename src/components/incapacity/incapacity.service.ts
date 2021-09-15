da29814db5a084e319972292ea3fe063611734f8
develop-joseavila <86316083+develop-joseavila@users.noreply.github.com>
2021-09-15 08:29:38 -0500

    Update incapacity  component 2021-09-15
    Update incapacity  component 2021-09-15

--- src/components/incapacity/incapacity.service.ts
+++ src/components/incapacity/incapacity.service.ts
@@ -1,9 +1,11 @@
 import { EpsRepository } from "../eps/repositories/eps.repository";
 import { IncapacityhRepository } from "./repositories/incapacity.repository";
-import fs from "fs";
-import path from "path";
 import { AlertaHtml, CreateOrValidateFolder, extraerExtensionArchivo, getDateToday, limpiarNombreArchivo } from "../common/helpers/global";
 import { mssqlBiplus, mssqlEsmad } from "../../services/mssql";
+import { uploadFileBlob } from "../../services/azure-blob";
+import fs from "fs";
+import path from "path";
+import { v4 } from "uuid";
 
 
 export class IncapacityService {
@@ -37,11 +39,14 @@ export class IncapacityService {
   }
 
 
+
   public async saveDisabilityFiling(reqData: any) {
     try {
 
       const { files, body } = reqData;
       const jsonBody = JSON.parse(body.allData);
+      let { dataUser, dataForm } = jsonBody;
+      const folder = "incapacity";
       let jsonFilesNames: any;
       let jsonFilesCodigos: any;
 
@@ -53,74 +58,112 @@ export class IncapacityService {
         jsonFilesCodigos = body.filesCodigos;
       }
 
-      let publicIncapacitiyFolder = path.join(__dirname, '..', '..', '..', 'temp', 'incapacity');
-      if (CreateOrValidateFolder(publicIncapacitiyFolder)) {
+      dataForm.otraEntidad.status = (dataForm.otraEntidad.status === true) ? 1 : 0;
+      dataForm.otraEntidad.value = (dataForm.otraEntidad.value === 'null') ? null : `'${dataForm.otraEntidad.value}'`;
+      dataForm.prorroga = (dataForm.prorroga === true) ? 1 : 0;
 
-        let publicUserFolder = path.join(__dirname, '..', '..', '..', 'temp', 'incapacity', jsonBody.dataUser.cedula);
-        if (CreateOrValidateFolder(publicUserFolder)) {
+      const saveDisabilityFiling = await this.incapacityRepository.saveDisabilityFiling(dataUser.cedula, dataUser.nombres, dataUser.telefono, dataUser.correoElectronico, dataUser.eps, dataForm.otraEntidad.status, dataForm.otraEntidad.value, dataForm.tipoIncapacidad, dataForm.rangoFechas.fechaInicio, dataForm.rangoFechas.fechaFin, dataUser.empresa, dataForm.prorroga);
+      const lastIdInsert = saveDisabilityFiling.INCAP_CODIGO;
 
-          let { dataUser, dataForm } = jsonBody;
+      if (lastIdInsert) {
 
-          dataForm.otraEntidad.status = (dataForm.otraEntidad.status === true) ? 1 : 0;
-          dataForm.otraEntidad.value = (dataForm.otraEntidad.value === 'null') ? null : `'${dataForm.otraEntidad.value}'`;
-          dataForm.prorroga = (dataForm.prorroga === true) ? 1 : 0;
+        let posibleError = 0;
+        files.map(async (file: Express.Multer.File, key: any) => {
 
-          const saveDisabilityFiling = await this.incapacityRepository.saveDisabilityFiling(
-            dataUser.cedula,
-            dataUser.nombres,
-            dataUser.telefono,
-            dataUser.correoElectronico,
-            dataUser.eps,
-            dataForm.otraEntidad.status,
-            dataForm.otraEntidad.value,
-            dataForm.tipoIncapacidad,
-            dataForm.rangoFechas.fechaInicio,
-            dataForm.rangoFechas.fechaFin,
-            dataUser.empresa,
-            dataForm.prorroga
-          );
+          let fileName = `${v4()}.${file.mimetype.split("/")[1]}`;
+          let folderRouteWithFile = `${folder}/${jsonBody.dataUser.cedula}/${fileName}`;
 
-          const lastIdInsert = saveDisabilityFiling.INCAP_CODIGO;
+          let { _response }: any = await uploadFileBlob(folderRouteWithFile, file.buffer);
 
-          if (lastIdInsert) {
+          if (_response.status === 201) {
 
-            let posibleError = 0;
+            let urlFile = _response.request.url.split('?')[0];
 
-            files.forEach(async (file: any, key: any) => {
+            let saveIncapacityFile = await this.incapacityRepository.saveIncapacityFile(Number(lastIdInsert), String(urlFile), String(dataUser.cedula), Number(jsonFilesCodigos[key]));
+            (!saveIncapacityFile) && (posibleError++);
 
-              let extension = extraerExtensionArchivo(file.originalname.toLowerCase());
-              let nameFile = `SolicitudNum_${lastIdInsert}_Cc_${jsonBody.dataUser.cedula}_${limpiarNombreArchivo(jsonFilesNames[key])}_${getDateToday(true)}.${extension}`;
-              let publicRoute = path.join(__dirname, '..', '..', '..', 'temp', 'incapacity', jsonBody.dataUser.cedula, nameFile);
-
-              if (!fs.createWriteStream(publicRoute).write(file.buffer)) {
-
-                let routeFile = path.join('temp', 'incapacity', jsonBody.dataUser.cedula, nameFile);
-                await this.incapacityRepository.saveIncapacityFile(Number(lastIdInsert), String(routeFile), String(dataUser.cedula), Number(jsonFilesCodigos[key]));
-
-              } else {
-                posibleError++;
-              }
-
-            });
-
-            if (posibleError === 0) {
-
-              // const correo = dataUser.correoElectronico;
-              const correo = `jose.avila@visionymarketing.com.co`;
-              const asunto = `RADICACION DOCUMENTOS`;
-              const titulo = `RADICACION DOCUMENTOS`;
-              const mensaje = `Su incapacidad ha sido radicada con el numero de consecutivo #${lastIdInsert}. la Cual se encuentra pendiente por aprobacion.<br> Al momento de que su Incapacidad sea aprobada o rechazada se le informara por correo electronico.`;
-              const copia = `jose.avila@visionymarketing.com.co`;
-              // const copia = `info.visionymarketing@visionymarketing.com.co`;
-              const body = AlertaHtml(titulo, mensaje);
-
-              const responseSendCorreo = this.insertarAlertarAutomaticas(correo, copia, asunto, body, '', mssqlBiplus);
-
-              return true;
-            }
           }
+
+        });
+
+
+        if (posibleError === 0) {
+
+          // const correo = dataUser.correoElectronico;
+          const correo = `jose.avila@visionymarketing.com.co`;
+          const asunto = `RADICACIÓN DOCUMENTOS`;
+          const titulo = `RADICACIÓN DOCUMENTOS`;
+          const mensaje = `Su incapacidad ha sido radicada con el numero de consecutivo #${lastIdInsert}. la Cual se encuentra pendiente por aprobacion.<br> Al momento de que su Incapacidad sea aprobada o rechazada se le informara por correo electronico.`;
+          const copia = `jose.avila@visionymarketing.com.co`;
+          // const copia = `info.visionymarketing@visionymarketing.com.co`;
+          const body = AlertaHtml(titulo, mensaje);
+
+          const responseSendCorreo = this.insertarAlertarAutomaticas(correo, copia, asunto, body, '', mssqlBiplus);
+
+          return true;
         }
+
       }
+
+      return false;
+
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async updateDisabilityFiling(reqData: any) {
+    try {
+
+      const { files, body } = reqData;
+      const { codigosArchivos, correoUsuario, numeroIncapacidad, cedulaUsuario } = body;
+
+      const folder = "incapacity";
+      let jsonFilesCodigos: any;
+
+      if (!Array.isArray(codigosArchivos)) {
+        jsonFilesCodigos = [codigosArchivos];
+      } else {
+        jsonFilesCodigos = codigosArchivos;
+      }
+
+      let posibleError = 0;
+      files.map(async (file: Express.Multer.File, key: any) => {
+
+        let fileName = `${v4()}.${file.mimetype.split("/")[1]}`;
+        let folderRouteWithFile = `${folder}/${cedulaUsuario}/${fileName}`;
+
+        let { _response }: any = await uploadFileBlob(folderRouteWithFile, file.buffer);
+
+        if (_response.status === 201) {
+
+          let urlFile = _response.request.url.split('?')[0];
+
+          const responseUpdatefiles = await this.incapacityRepository.updateFilesIncapacity(jsonFilesCodigos[key], urlFile, numeroIncapacidad);
+          (!responseUpdatefiles) && (posibleError++);
+
+        }
+
+      });
+
+
+      if (posibleError === 0) {
+
+        // const correo = correoUsuario;
+        const correo = `jose.avila@visionymarketing.com.co`;
+        const asunto = `RADICACIÓN DOCUMENTOS`;
+        const titulo = `RADICACIÓN DOCUMENTOS`;
+        const mensaje = `Lo documentos para su incapacidad radicada con el número #${numeroIncapacidad} fueron subidos nuevamente para su aprobación.`;
+        const copia = `jose.avila@visionymarketing.com.co`;
+        // const copia = `info.visionymarketing@visionymarketing.com.co`;
+        const body = AlertaHtml(titulo, mensaje);
+
+        const responseSendCorreo = this.insertarAlertarAutomaticas(correo, copia, asunto, body, '', mssqlBiplus);
+
+        return true;
+      }
+
+
 
       return false;
 
